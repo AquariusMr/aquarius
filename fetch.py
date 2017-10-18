7118fad12c25a7b282f2559a540fff9d7f5d8a85
Your Name <you@example.com>
2017-10-19 02:53:03 +0800

    fix

--- fetch.py
+++ fetch.py
@@ -11,43 +11,49 @@ class HTTPRequest(object):
         return cls._instance
 
     def __init__(self, uri=None, method=None):
-        self._request_header = ('%(method)s /%(uri)s HTTP/1.0\r\nHost: %(host)s\r\n\r\n')
+        self._request_header = ('%(method)s /%(path)s HTTP/1.0\r\n' 
+                                'Host: %(host)s\r\n\r\n')
         self._request_body = '%(body)s\r\n'
 
-        self._uri = uri
-        self._method = method
+    @staticmethod
+    def uri_parse(uri):
+        uris = uri.rsplit("/", 1)
 
-    def __call__(self, method=None, uri=None):
-        if uri:
-            self._uri = uri
+        if len(uris) > 1:
+            path = uris[1]
+        else:
+            path = ""
 
-        if method is None:
-            pass
-        elif method is "GET":
-            return self.get()
+        return {'host': uris[0], "path": path}
+
+
+    def __call__(self, method="GET", uri=None):
+        kwargs = self.uri_parse(uri)
+        kwargs["method"] = method
 
         return self
 
     async def fetch(self, uri, port=80, method='GET'):
+        kwargs = self.uri_parse(uri)
+        kwargs["method"] = method
 
-        host = uri.split("?", 1)[0]
+        request_string = self._request_header % kwargs
 
-        response_header = {}
-        response_body = b''
 
         connect = asyncio.open_connection(uri, port)
-
         reader, writer = await connect
-
-        request_string = self._request_header % {'method': 'GET', 'uri': uri , 'host': host}
 
         writer.write(request_string.encode('utf-8'))
 
         await writer.drain()
 
+        response_header = {}
+        response_body = b''
+
         response_line = await reader.readline()
 
         first_header = response_line.rstrip()
+        print(first_header)
         response_header[b'first_line'] = first_header
 
         protocol, status = first_header.split(b' ', 1)
@@ -75,22 +81,13 @@ class HTTPRequest(object):
 
         return {'header': response_header, 'body': response_body}
 
-    async def get(self, uri=None, port=80):
-        if uri is None:
-            uri = self._uri if self._uri else "0.0.0.0"
-        response = await self.fetch(uri, port=port)
-        return response['body']
-
-    async def post(self, uri, port=80, method='POST'):
-        pass
-
 
 
 if __name__ == '__main__':
+    requests = HTTPRequest()
 
     loop = asyncio.get_event_loop()
-    uri = """api.weixin.qq.com/sns/oauth2/access_token?appid=wx9aac9c21368447e6&secret=462a3ca630034191f0a5faacd75aa60f&code=011Uc5tA0RM8ue27phsA0qn8tA0Uc5tS&state&grant_type=authorization_code"""
 
-    tasks = [HTTPRequest(uri)("GET")]
+    tasks = [requests.fetch("www.baidu.com", 443)]
     loop.run_until_complete(asyncio.wait(tasks))
     loop.close()
